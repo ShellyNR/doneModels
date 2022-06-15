e1295bfc7703a242f49a6549cbfdf9ecacb1a7e8
ShellyNR <73890534+ShellyNR@users.noreply.github.com>
2022-06-15 04:31:36 +0300

    Update run_predictions.py

--- run_predictions.py
+++ run_predictions.py
@@ -149,6 +149,9 @@ be = Flask(__name__)
 # both - text and photos endpoint
 @be.route('/', methods=['POST'])
 def hello():
+    with open('resp.json', 'r') as f:
+        data = json.load(f)
+    return data
     # print(request.get_json()["description"])
     # return "Hello World!"
     print("in be server")
@@ -172,8 +175,8 @@ def hello():
 
 
 if __name__ == '__main__':
-#     be.run(host='0.0.0.0', port=8000,debug=True)
-    calc_preds()
+    be.run(host='0.0.0.0', port=8000,debug=True)
+#     calc_preds()
 
 
 
