fc99a4ae020926c3bf7459f2957a0073664acfb5
snahir <shelly.nahir@live.biu.ac.il>
2022-06-15 04:42:20 +0300

    Merge remote-tracking branch 'origin/master'

---
+++ dummy.py
@@ -0,0 +1,17 @@
+import json
+
+from flask import Flask
+
+be = Flask(__name__)
+
+
+# both - text and photos endpoint
+@be.route('/', methods=['POST'])
+def hello():
+    with open('resp.json', 'r') as f:
+        data = json.load(f)
+    return data
+
+
+if __name__ == '__main__':
+    be.run(host='0.0.0.0', port=8000,debug=True)
