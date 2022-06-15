8a8d1170aa578305e3df5d4311cf69e862506b73
snahir <shelly.nahir@live.biu.ac.il>
2022-06-16 00:05:40 +0300

    Merge remote-tracking branch 'origin/master'

--- run_predictions.py
+++ run_predictions.py
@@ -103,7 +103,7 @@ def calc_preds():
     dict["i_messy_rate"] = messy_rates
     dict["i_triq_model"] = triq_pred()
     dict["i_blur_rate"] = blur_detect()
-    dict["i_fake_rate"] = detect_manupulation()
+    # dict["i_fake_rate"] = detect_manupulation()
     dict["grammar_model"] = text_model(description)
     dict["buzzwords_model"] = check_text_quality(description)
 
