47c0af45b15fae54b2fa48726c6d22f67828bb88
snahir <shelly.nahir@live.biu.ac.il>
2022-06-16 05:05:18 +0300

    Merge remote-tracking branch 'origin/master'

--- run_predictions.py
+++ run_predictions.py
@@ -161,7 +161,7 @@ def hello():
 if __name__ == '__main__':
     # be.run(host='0.0.0.0', port=8000,debug=True)
     # calc_preds()
-    isMessy()
+    triq_pred()
 
 
 #temp_function_user_simulator("../dark_vs_bright_model/assets/dark.txt")
