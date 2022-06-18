e3770311f9f1fcbfc1a494c13f049bb088a5005b
naamasharir <naamasharir3@gmail.com>
2022-06-18 03:09:14 +0300

    print calc_preds output

--- run_predictions.py
+++ run_predictions.py
@@ -161,7 +161,7 @@ def removeTemp():
 
 if __name__ == '__main__':
     # be.run(host='0.0.0.0', port=8000,debug=True)
-    calc_preds()
+    print(calc_preds())
     # isMessy()
 
 #temp_function_user_simulator("../dark_vs_bright_model/assets/dark.txt")
