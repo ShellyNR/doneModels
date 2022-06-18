400da3af40055a9c79042540452092887bee7321
Your Name <you@example.com>
2022-06-18 19:03:06 +0300

    arrange code

--- run_predictions.py
+++ run_predictions.py
@@ -1,68 +1,22 @@
+import PIL.Image as Image
 import glob
 import shutil
+import json
+import base64
+import io
+import os
 
-
-import cv2
 from dark_vs_bright_model.run import isBright
 from tidyDetection.tidy_detection import tidy_detect
 from image_manipulation_detection.detect_manipulation import detect_manupulation
-from messy_room_classifier_master.predict import isMessy
 from triq.image_quality_prediction import triq_pred
 from roomTypeModel.roomType_detection import roomType_model
-import json
-import numpy as np
-from download_images import get
 from textModel.textModel import text_model
 from blurDetection.blur_detection import blur_detect
-import io
-import os
-import PIL.Image as Image
-import base64
 from check_BuzzWords.checkBuzzWords import check_text_quality
 from sentiment_model.sentiment import sentiments_model
 
-
-# class Echo(protocol.Protocol):
-#     """This is just about the simplest possible protocol"""
-#
-#
-#     def dataReceived(self, data):
-#         "As soon as any data is received, write it back."
-#         j = json.loads(data.decode('utf-8'))
-#         print(j["description"])
-#         f = open('resp.json')
-#         self.transport.write(f.read().encode('utf-8'))
-#         f.close()
-#
-#
-# def main():
-#     """This runs the protocol on port 8000"""
-#     factory = protocol.ServerFactory()
-#     factory.protocol = Echo
-#     reactor.listenTCP(8000, factory)
-# #     reactor.run()
-#
-#
-# # this only runs if the module was *not* imported
-# if __name__ == '__main__':
-#     main()
-
 from flask import Flask, request
-
-def load_from_url(url):
-    get(url)
-
-def decode_images():
-    with open('t.text') as f:
-        image = f.read()
-    with open("imageToSave.png", "wb") as fh:
-        fh.write(base64.decodebytes(image))
-    #im = bytes(image, encoding='utf-8')
-    im = str(base64.decodebytes(bytes(image, encoding='utf-8')))
-    path = os.path.join("images/rr" + ".jpg")
-    im.save(path)
-    f.close()
-    return;
 
 def calc_preds(description):
     dict = {
@@ -104,15 +58,15 @@ def calc_preds(description):
 
         # dict["i_fake_rate"] = detect_manupulation()
 
+    if len(description) != 0:
+        dict["grammar_model"] = text_model(description)
+        print("model grammar done")
 
-    dict["grammar_model"] = text_model(description)
-    print("model grammar done")
+        dict["sentiment_model"] = sentiments_model(description)
+        print("model sentiment done")
 
-    dict["sentiment_model"] = sentiments_model(description)
-    print("model sentiment done")
-
-    dict["buzzwords_model"] = check_text_quality(description)
-    print("model buzzwords done")
+        dict["buzzwords_model"] = check_text_quality(description)
+        print("model buzzwords done")
 
     with open('resp.json', 'w') as f:
         json_object = json.dumps(dict, indent=4)
@@ -120,36 +74,24 @@ def calc_preds(description):
         f.close()
     return json_object
 
-def temp_function_user_simulator(url_file):
-    path= "images/"
-    os.makedirs(path, exist_ok=True)
-    with open(url_file, "r") as f:
-        for url in f.read().split("\n"):
-            try:
-                get(url, path=path)
-            except Exception as e:
-                print(e)
-
 def resizeInTemp(path):
     im = Image.open(path)
     # resizedImage = im.resize((1024, 768))
     resizedImage = im.resize((512, 384))
     name = os.path.basename(path)
     imgPath = os.path.join("temp/" + name)
-    os.makedirs(os.path.dirname(imgPath), exist_ok=True)
     resizedImage.save(imgPath)
-    return
 
 def resetDirs(path):
     forImages = path + "/images"
-    forTemp = path + "/temp"
     if os.path.exists(forImages):
         shutil.rmtree(forImages)
     os.mkdir(forImages)
+
+    forTemp = path + "/temp"
     if os.path.exists(forTemp):
         shutil.rmtree(forTemp)
     os.mkdir(forTemp)
-
 
 be = Flask(__name__)
 @be.route('/', methods=['POST'])
