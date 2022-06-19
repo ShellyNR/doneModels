e4233409b07dc0bd27b3a001cdbe70ba97780593
Your Name <you@example.com>
2022-06-19 19:47:44 +0300

    upload roomType model

--- roomType_model/roomType.py
+++ roomType_model/roomType.py
@@ -1,3 +1,6 @@
+# ! wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=10SrowcOJp8GWqEB21BfCIinqUCHS7PMv' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=10SrowcOJp8GWqEB21BfCIinqUCHS7PMv" -O cls_house_scene_trained.zip && rm -rf /tmp/cookies.txt
+# ! unzip -qq cls_house_scene_trained.zip
+
 from monk.gluon_prototype import prototype
 import os
 import string
