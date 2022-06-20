d696400ceeb8517ea0a54b238ec0f5ebf4ba2be0
naamasharir <naamasharir3@gmail.com>
2022-06-21 00:55:34 +0300

    cleaned sentiments model code

--- sentiment_model/sentiment.py
+++ sentiment_model/sentiment.py
@@ -7,87 +7,94 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
 
+# Constants
+PATH = 'sentiment_data/opinions.tsv'
+LANGUAGE = 'english'
+PERCENTAGES = 100
+HIGH_DESCRIPTION = "The description sentiment is great!"
+MEDIUM_DESCRIPTION = "The description sentiment is positive, but you can consider improving it a little."
+LOW_DESCRIPTION = "The description sentiment is negative, try using positive words to describe the property."
+
 
 def train_all_models():
-    X_train_dtm, y_train, X_test_dtm, y_test, X_test, vect, X, y = load_data()
-    naive_bayes_model(X_train_dtm, y_train, X_test_dtm, y_test)
-    logistic_regression_model(X_train_dtm, y_train, X_test_dtm, y_test)
-    svm_model(X_train_dtm, y_train, X_test_dtm, y_test)
-    knn_model(X_train_dtm, y_train, X_test_dtm, y_test)
-    return X, y
+    x_train_dtm, y_train, x_test_dtm, y_test, x_test, vector, x, y = load_data()
+    naive_bayes_model(x_train_dtm, y_train, x_test_dtm, y_test)
+    logistic_regression_model(x_train_dtm, y_train, x_test_dtm, y_test)
+    svm_model(x_train_dtm, y_train, x_test_dtm, y_test)
+    knn_model(x_train_dtm, y_train, x_test_dtm, y_test)
+    return x, y
 
 def load_data():
-    path = 'sentiment_model/sentiment_data/opinions.tsv'
-    data = pd.read_table(path, header=None, skiprows=1, names=['Sentiment', 'Review'])
-    X = data.Review
+    data = pd.read_table(PATH, header=None, skiprows=1, names=['Sentiment', 'Review'])
+    x = data.Review
     y = data.Sentiment
 
     # Using CountVectorizer to convert text into tokens/features
-    vect = CountVectorizer(stop_words='english', ngram_range=(1, 1), max_df=.80, min_df=4)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1, test_size=0.2)
+    vector = CountVectorizer(stop_words=LANGUAGE, ngram_range=(1, 1), max_df=.80, min_df=4)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=1, test_size=0.2)
 
     # Using training data to transform text into counts of features for each message
-    vect.fit(X_train)
-    X_train_dtm = vect.transform(X_train)
-    X_test_dtm = vect.transform(X_test)
+    vector.fit(x_train)
+    x_train_dtm = vector.transform(x_train)
+    x_test_dtm = vector.transform(x_test)
 
-    return X_train_dtm, y_train, X_test_dtm, y_test, X_test, vect, X, y
+    return x_train_dtm, y_train, x_test_dtm, y_test, x_test, vector, x, y
 
 # Accuracy using Naive Bayes Model
-def naive_bayes_model(X_train_dtm, y_train, X_test_dtm, y_test):
-    NB = MultinomialNB()
-    NB.fit(X_train_dtm, y_train)
-    y_pred = NB.predict(X_test_dtm)
-    accuracy = metrics.accuracy_score(y_test, y_pred)*100
+def naive_bayes_model(x_train_dtm, y_train, x_test_dtm, y_test):
+    naive_bayes = MultinomialNB()
+    naive_bayes.fit(x_train_dtm, y_train)
+    y_prediction = naive_bayes.predict(x_test_dtm)
+    accuracy = metrics.accuracy_score(y_test, y_prediction) * PERCENTAGES
     print('\nNaive Bayes')
     print('Accuracy Score: ', accuracy, '%', sep='')
-    return accuracy, NB, y_pred
+    return accuracy, naive_bayes, y_prediction
 
 # Accuracy using Logistic Regression Model
-def logistic_regression_model(X_train_dtm, y_train, X_test_dtm, y_test):
-    LR = LogisticRegression()
-    LR.fit(X_train_dtm, y_train)
-    y_pred = LR.predict(X_test_dtm)
-    accuracy = metrics.accuracy_score(y_test, y_pred)*100
+def logistic_regression_model(x_train_dtm, y_train, x_test_dtm, y_test):
+    logistic_regression = LogisticRegression()
+    logistic_regression.fit(x_train_dtm, y_train)
+    y_prediction = logistic_regression.predict(x_test_dtm)
+    accuracy = metrics.accuracy_score(y_test, y_prediction) * PERCENTAGES
     print('\nLogistic Regression')
     print('Accuracy Score: ', accuracy, '%', sep='')
     return accuracy
 
 # Accuracy using SVM Model
-def svm_model(X_train_dtm, y_train, X_test_dtm, y_test):
+def svm_model(x_train_dtm, y_train, x_test_dtm, y_test):
     SVM = LinearSVC()
-    SVM.fit(X_train_dtm, y_train)
-    y_pred = SVM.predict(X_test_dtm)
-    accuracy = metrics.accuracy_score(y_test, y_pred)*100
+    SVM.fit(x_train_dtm, y_train)
+    y_prediction = SVM.predict(x_test_dtm)
+    accuracy = metrics.accuracy_score(y_test, y_prediction) * PERCENTAGES
     print('\nSupport Vector Machine')
     print('Accuracy Score: ', accuracy, '%', sep='')
     return accuracy
 
 # Accuracy using KNN Model
-def knn_model(X_train_dtm, y_train, X_test_dtm, y_test):
+def knn_model(x_train_dtm, y_train, X_test_dtm, y_test):
     KNN = KNeighborsClassifier(n_neighbors=3)
-    KNN.fit(X_train_dtm, y_train)
-    y_pred = KNN.predict(X_test_dtm)
-    accuracy = metrics.accuracy_score(y_test, y_pred)*100
+    KNN.fit(x_train_dtm, y_train)
+    y_prediction = KNN.predict(X_test_dtm)
+    accuracy = metrics.accuracy_score(y_test, y_prediction) * PERCENTAGES
     print('\nK Nearest Neighbors (NN = 3)')
     print('Accuracy Score: ', accuracy, '%', sep='')
     return accuracy
 
 # Custom Test: Test a review on the best performing model (Logistic Regression)
-def logistic_regression_train(X, y):
-    trainingVector = CountVectorizer(stop_words='english', ngram_range=(1, 1), max_df=.80, min_df=5)
-    trainingVector.fit(X)
-    X_dtm = trainingVector.transform(X)
-    LR_complete = LogisticRegression()
-    LR_complete.fit(X_dtm, y)
+def logistic_regression_train(x, y):
+    training_vector = CountVectorizer(stop_words=LANGUAGE, ngram_range=(1, 1), max_df=.80, min_df=5)
+    training_vector.fit(x)
+    x_dtm = training_vector.transform(x)
+    logistic_regression = LogisticRegression()
+    logistic_regression.fit(x_dtm, y)
 
-    return trainingVector, LR_complete
+    return training_vector, logistic_regression
 
 # infer function on given description
-def infer(description, trainingVector, LR_complete):
+def infer(description, training_vector, logistic_regression):
     text = [description]
-    text_dtm = trainingVector.transform(text)
-    prediction = LR_complete.predict_proba(text_dtm)
+    text_dtm = training_vector.transform(text)
+    prediction = logistic_regression.predict_proba(text_dtm)
 
     # tags = ['Negative','Positive']
     # Display Output
@@ -98,18 +105,17 @@ def infer(description, trainingVector, LR_complete):
 
 # the function gets a grade (0 - 1) and returns a sentence regarding the grade
 def sentence_grade(grade):
-    grade = grade * 100
+    grade = grade * PERCENTAGES
     if grade > 85:
-        return "The description sentiment is great!"
+        return HIGH_DESCRIPTION
     if grade > 60:
-        return "The description sentiment is positive, but you can consider improving it a little."
+        return MEDIUM_DESCRIPTION
     else:
-        return "The description sentiment is negative, try using positive words to describe the property."
+        return LOW_DESCRIPTION
 
 def sentiments_model(description):
-    # X, y = train_all_models()
-    X_train_dtm, y_train, X_test_dtm, y_test, X_test, vect, X, y = load_data()
-    trainingVector, LR_complete = logistic_regression_train(X, y)
-    grade = infer(description, trainingVector, LR_complete)
+    # x, y = train_all_models()
+    x_train_dtm, y_train, x_test_dtm, y_test, x_test, vector, x, y = load_data()
+    training_vector, logistic_regression = logistic_regression_train(x, y)
+    grade = infer(description, training_vector, logistic_regression)
     return [grade, sentence_grade(grade)]
-
