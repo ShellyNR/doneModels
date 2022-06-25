2b1b63c5c9622809e0eb4e1fe52ae4a6a404a8f2
naamasharir <naamasharir3@gmail.com>
2022-06-26 00:13:47 +0300

    changed code in sentiment model

--- sentiment_model/train.py
+++ sentiment_model/train.py
@@ -10,40 +10,44 @@ import pickle
 
 
 # Constants
-PATH = 'sentiment_model/sentiment_data/opinions.tsv'
+DATASET_PATH = 'sentiment_model/sentiment_data/opinions.tsv'
+TRAINING_VECTOR_FILENAME = 'training_vector.sav'
+LOGISTIC_REGRESSION_FILENAME = 'logistic_regression.sav'
 LANGUAGE = 'english'
 PERCENTAGES = 100
 K_KNN = 3
 TEST_SIZE = 0.2
+MAX_DF = .80
+MIN_DF = 5
 
 # train all optional models and prints their accuracies
 def train_all_models():
     values = load_data()
+    split_data(values)
     train_model(values, MultinomialNB(), 'Naive Bayes')
     train_model(values, LogisticRegression(), 'Logistic Regression')
     train_model(values, LinearSVC(), 'Support Vector Machine (SVM)')
     train_model(values, KNeighborsClassifier(n_neighbors=K_KNN), 'K Nearest Neighbors')
 
-    return values['x'], values['y']
+
+def split_data(values):
+    values['x_train'], values['x_test'], values['y_train'], values['y_test'] = \
+        train_test_split(values['x'], values['y'], random_state=1, test_size=TEST_SIZE)
+
+    # Using CountVectorizer to convert text into tokens/features
+    vector = CountVectorizer(stop_words=LANGUAGE, ngram_range=(1, 1), max_df=MAX_DF, min_df=MIN_DF)
+    vector.fit(values['x_train'])
+    values['x_train_dtm'] = vector.transform(values['x_train'])
+    values['x_test_dtm'] = vector.transform(values['x_test'])
+    values['vector'] = vector
+
 
 # loads the data from the dataset and defines the train and validation
 def load_data():
     values = dict()
-    data = pd.read_table(PATH, header=None, skiprows=1, names=['Sentiment', 'Review'])
+    data = pd.read_table(DATASET_PATH, header=None, skiprows=1, names=['Sentiment', 'Review'])
     values['x'] = data.Review
     values['y'] = data.Sentiment
-
-    # Using CountVectorizer to convert text into tokens/features
-    values['vector'] = CountVectorizer(stop_words=LANGUAGE, ngram_range=(1, 1), max_df=.80, min_df=4)
-    x_train, x_test, y_train, y_test = train_test_split(values['x'], values['y'], random_state=1, test_size=TEST_SIZE)
-    values['x_train'] = x_train
-    values['x_test'] = x_test
-    values['y_train'] = y_train
-    values['y_test'] = y_test
-    # Using training data to transform text into counts of features for each message
-    values['vector'].fit(x_train)
-    values['x_train_dtm'] = values['vector'].transform(x_train)
-    values['x_test_dtm'] = values['vector'].transform(x_test)
 
     return values
 
@@ -60,13 +64,12 @@ def train_model(values, model, model_name):
 # Train the best performing model (Logistic Regression)
 def logistic_regression_train():
     values = load_data()
-    training_vector = CountVectorizer(stop_words=LANGUAGE, ngram_range=(1, 1), max_df=.80, min_df=5)
+    training_vector = CountVectorizer(stop_words=LANGUAGE, ngram_range=(1, 1), max_df=MAX_DF, min_df=MIN_DF)
     training_vector.fit(values['x'])
     x_dtm = training_vector.transform(values['x'])
+
     logistic_regression = LogisticRegression()
     logistic_regression.fit(x_dtm, values['y'])
-    training_vector_filename = 'training_vector.sav'
-    pickle.dump(training_vector, open(training_vector_filename, 'wb'))
 
-    logistic_regression_filename = 'logistic_regression.sav'
-    pickle.dump(logistic_regression, open(logistic_regression_filename, 'wb'))
+    pickle.dump(training_vector, open(TRAINING_VECTOR_FILENAME, 'wb'))
+    pickle.dump(logistic_regression, open(LOGISTIC_REGRESSION_FILENAME, 'wb'))
